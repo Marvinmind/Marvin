f10adc1b5ecaaf4772446da2fb6996e21e0009c7
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2016-05-13 12:11:16 +0200

    Debugging conducted. Conversations possible in principle

--- run.py
+++ run.py
@@ -10,141 +10,148 @@ import atexit
 from messageSimple import Message
 from queue import Queue
 
+
 class RecThread(QThread):
-	receive_signal = pyqtSignal(Message)
-	def __init__(self, sock):
-		QThread.__init__(self)
-		self.sock = sock
-	def run(self):
-		while 1:
-			m = self.sock.receive_message()
-			if m:
-				print('emit message')
-				self.receive_signal.emit(m)
+
+    receive_signal = pyqtSignal(Message)
+    def __init__(self, sock):
+        QThread.__init__(self)
+        self.sock = sock
+    def run(self):
+        while 1:
+            m = self.sock.receive_message()
+            if m:
+                print('emit message')
+                self.receive_signal.emit(m)
+
 
 class SendThread(QThread):
-	def __init__(self, sock, queue):
-		QThread.__init__(self)
-		self.sock = sock
-		self.queue = queue
-	def run(self):
-		while 1:
-			val= self.queue.get()
-			if val is not None:
-				self.sock.send_message(val)
+    def __init__(self, sock, queue):
+        QThread.__init__(self)
+        self.sock = sock
+        self.queue = queue
+
+    def run(self):
+        while 1:
+            val= self.queue.get()
+            if val is not None:
+                self.sock.send_message(val)
+
 
 class ListenThread(QThread):
-	incoming_connection_signal = pyqtSignal(ClientSock)
-	def __init__(self, port):
-		QThread.__init__(self)
-		self.serverSock = ServerSock(port)
+    incoming_connection_signal = pyqtSignal(ClientSock)
 
-	def run(self):
-		while 1:
-			conn = self.serverSock.getConnection()
-			self.clientSock = ClientSock(serverPort)
-			self.clientSock.s = conn
-			print('connection established')
-			self.incoming_connection_signal.emit(self.clientSock)
-			#win = ConvWindow(socket=self.clientSock)
-			#win.show()
+    def __init__(self, port):
+        QThread.__init__(self)
+        self.serverSock = ServerSock(port)
+
+    def run(self):
+        while 1:
+            conn = self.serverSock.getConnection()
+            self.clientSock = ClientSock(serverPort)
+            self.clientSock.s = conn
+            print('connection established')
+            self.incoming_connection_signal.emit(self.clientSock)
+        #win = ConvWindow(socket=self.clientSock)
+        #win.show()
 
 
 class ConvWindow(QtWidgets.QMainWindow, Ui_Marvin):
-	def __init__(self, **kwargs):
-		super(ConvWindow, self).__init__(None)
-		self.setupUi(self)	
+    def __init__(self, **kwargs):
+        super(ConvWindow, self).__init__(None)
+        self.setupUi(self)
 
-		#Check if socket hat been given. If not create socket from address.
-		if 'socket' in kwargs:
-			print('socket transfered')
-			self.sock = kwargs['socket']
-		else:
-			self.sock = ClientSock(serverPort)
-			self.sock.connect(kwargs['addr'])
+        #Check if socket hat been given. If not create socket from address.
+        if 'sock' in kwargs:
+            print('socket transfered')
+            self.sock = kwargs['sock']
+        else:
+            self.sock = ClientSock(serverPort)
+            self.sock.connect(int(kwargs['addr']))
 
-		#Set up sender thread
-		self.queue = Queue()
-		self.send_thread = SendThread(self.sock, self.queue)
-		self.send_thread.start()
+        #Set up sender thread
+        self.queue = Queue()
+        self.send_thread = SendThread(self.sock, self.queue)
+        self.send_thread.start()
 
-		#Set up receiver thread
-		self.get_thread = RecThread(self.sock)
-		self.get_thread.start()
-		self.get_thread.receive_signal.connect(self.addToChat)
+        #Set up receiver thread
+        self.get_thread = RecThread(self.sock)
+        self.get_thread.start()
+        self.get_thread.receive_signal.connect(self.addToChat)
 
-		self.pushButton.clicked.connect(self.sendMessage)
-		self.pushButton.clicked.connect(self.lineEdit.clear)
+        self.pushButton.clicked.connect(self.sendMessage)
+        self.pushButton.clicked.connect(self.lineEdit.clear)
 
-		self.chatListModel = QtGui.QStandardItemModel()
-		self.listView.setModel(self.chatListModel)
+        self.chatListModel = QtGui.QStandardItemModel()
+        self.listView.setModel(self.chatListModel)
 
-	def addToChat(self, m):
-		m_string = 'From: {}\nMessage: \n{}\nStatus: {}\n'.format(m.sender,m.body, m.verify())
-		item = QtGui.QStandardItem(m_string)
-		item.setForeground(QtGui.QColor('red'))
-		self.chatListModel.appendRow(item)
+    def addToChat(self, m):
+        m_string = 'From: {}\nMessage: \n{}\nStatus: {}\n'.format(m.sender,m.body, m.verify())
+        item = QtGui.QStandardItem(m_string)
+        item.setForeground(QtGui.QColor('red'))
+        self.chatListModel.appendRow(item)
 
-	def sendMessage(self):
-		message = self.lineEdit.text()
-		item = QtGui.QStandardItem(str(message)+'\n')
-		item.setForeground(QtGui.QColor('blue'))
-		self.chatListModel.appendRow(item)
-		self.queue.put(message)
+    def sendMessage(self):
+        message = self.lineEdit.text()
+        item = QtGui.QStandardItem(str(message)+'\n')
+        item.setForeground(QtGui.QColor('blue'))
+        self.chatListModel.appendRow(item)
+        self.queue.put(message)
+
 
 class Main(QtWidgets.QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		super(Main, self).__init__(None)
-		self.setupUi(self)
+    def __init__(self):
+        super(Main, self).__init__(None)
+        self.setupUi(self)
 
-		self.listenThread = ListenThread(serverPort)
-		self.listenThread.start()
-		self.listenThread.incoming_connection_signal.connect(lambda x: self.openConversation(sock=x))
+        self.listenThread = ListenThread(serverPort)
+        self.listenThread.start()
+        self.listenThread.incoming_connection_signal.connect(lambda x: self.openConversation(sock=x))
 
-		self.convWindows = []
-		self.contacts = Contacts(clientNumber).contacts
-		for c in self.contacts:
-			el = ''
-			if 'name' in c:
-				el = c['name']
-			elif 'address' in c:
-				el = c['address']
-			if el != '':
-				item = self.listWidget.addItem(el)
-		self.listWidget.itemDoubleClicked.connect(lambda: self.openConversation(addr=c['address']))
+        self.convWindows = []
+        self.contacts = Contacts(clientNumber).contacts
+        for c in self.contacts:
+            el = ''
+            if 'name' in c:
+                el = c['name']
+            elif 'address' in c:
+                el = c['address']
+            if el != '':
+                item = self.listWidget.addItem(el)
+        self.listWidget.itemDoubleClicked.connect(lambda: self.openConversation(addr=c['address']))
 
-	def openConversation(self, **kwargs):
-		if 'sock' in kwargs:
-			self.win = ConvWindow(sock=kwargs['sock'])
+    def openConversation(self, **kwargs):
+        if 'sock' in kwargs:
+            self.win = ConvWindow(sock=kwargs['sock'])
 
-		elif 'addr' in kwargs:
-			if not kwargs['addr'] in self.convWindows:
-				addr = kwargs['addr']
-				self.convWindows.append(addr)
-				self.win = ConvWindow(addr=addr)
-				
-		self.win.show()
+        elif 'addr' in kwargs:
+            if not kwargs['addr'] in self.convWindows:
+                addr = kwargs['addr']
+                self.convWindows.append(addr)
+                self.win = ConvWindow(addr=addr)
 
-	def openConversation2(self, item):
-			self.win= ConvWindow(socket=item)
-			self.win.show()
+        self.win.show()
+
+    def openConversation2(self, item):
+        self.win= ConvWindow(socket=item)
+        self.win.show()
 
 if __name__ == '__main__':
-	try:
-		app = QtWidgets.QApplication(sys.argv)
-		clientNumber = int(sys.argv[1])
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+        clientNumber = int(sys.argv[1])
 
-		if clientNumber==1:
-			serverPort=5555
-		elif clientNumber == 2:
-			serverPort=5556
-		else:
-			serverPort=5557
+        if clientNumber==1:
+            serverPort=5555
+        elif clientNumber == 2:
+            serverPort=5556
+        else:
+            serverPort=5557
 
-		prog = Main()
-		prog.show()
-		sys.exit(app.exec_())
-	finally	:
-		pass
-		#if socket:
-		#	socket.kill_sock()
+        prog = Main()
+        prog.show()
+        sys.exit(app.exec_())
+    finally	:
+        pass
+    #if socket:
+    #	socket.kill_sock()
--- socketClient.py
+++ socketClient.py
@@ -16,8 +16,8 @@ SOCKS_PORT = 7000
 #socket.socket = socks.socksocket
 
 def print_bootstrap_lines(line):
-  if "Bootstrapped " in line:
-    print(term.format(line, term.Color.BLUE))
+    if "Bootstrapped " in line:
+        print(term.format(line, term.Color.BLUE))
 
 #tor_process = stem.process.launch_tor_with_config(
 #  config = {
@@ -27,119 +27,120 @@ def print_bootstrap_lines(line):
 #)
 
 
-
 class MarvinSock():
-	def __init__(self, flav):
-		self.HOST = 'localhost'    # The remote host
-		self.PORT = 55555      # The same port as used by the server		
-		if flav == 1:
-			self.path = '/var/lib/tor/other_hidden_service'
-		else:
-			self.path = '/var/lib/tor/hidden_service'
 
-		self.gen = MessageCreator(self.path)
+    def __init__(self, flav):
+        self.HOST = 'localhost'    # The remote host
+        self.PORT = 55555      # The same port as used by the server
+        if flav == 1:
+            self.path = '/var/lib/tor/other_hidden_service'
+        else:
+            self.path = '/var/lib/tor/hidden_service'
 
-		try:
-			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			res = self.s.connect_ex((self.HOST, self.PORT))
-			if res!=0:
-				self.s.bind((self.HOST, self.PORT))
-				self.s.listen(1)
-				self.s, addr = self.s.accept()
-		finally:
-			pass
-		#finally:	
-		#	s.shutdown(socket.SHUT_RDWR)
-		#	s.close()
-		#	tor_process.kill() 
-	def receive_message(self):
-		d = self.s.recv(32)
-		if len(d)==0:
-			return
-		print('something came in')
-		messageLen = int(d[:4].decode('ascii'))
-		d = d[4:]
-		messageLen = int(messageLen)
-		while 1:
-			if len(d)<messageLen:
-				d+=self.s.recv(min(1024, messageLen-len(d)))
-			else:
-				break
-		if '---stop---' == d:
-			return
-		message = pickle.loads(d)
-		return message
+        self.gen = MessageCreator(self.path)
 
-	def send_message(self, m_in):
-		message = self.gen.buildMessage(m_in, self.sPort)
-		output = pickle.dumps(message, -1)
-		#calculate length of message and pad to four digits
-		length = str(len(output))
-		if len(length) > 4:
-			raise Exception('MessageLenException', 'this message is too long')
-		length = ''.join(['0' for x in range(4-len(length))])+length
-		print('goes out!')
-		self.s.sendall(length.encode('ascii')+output)
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            res = self.s.connect_ex((self.HOST, self.PORT))
+            if res!=0:
+                self.s.bind((self.HOST, self.PORT))
+                self.s.listen(1)
+                self.s, addr = self.s.accept()
+        finally:
+            pass
+        #finally:
+        #	s.shutdown(socket.SHUT_RDWR)
+        #	s.close()
+        #	tor_process.kill()
+    def receive_message(self):
 
-	def send_worker(self):
-		while 1:
-			m = input()
-			if m == '---end---':
-				kill_sock()
-			else:
-				self.send_message(m)
-	def receive_worker(self):
-		while 1:
-			m = self.receive_message()
-			if m == '---end---':
-				kill_sock()
-			print(m.body)
-	def kill_sock(self):
-		self.s.shutdown(socket.SHUT_RDWR)
-		self.s.close()
+        d = self.s.recv(32)
+        if len(d)==0:
+            return
+        print('something came in')
+        messageLen = int(d[:4].decode('ascii'))
+        d = d[4:]
+        messageLen = int(messageLen)
+        while 1:
+            if len(d)<messageLen:
+                d+=self.s.recv(min(1024, messageLen-len(d)))
+            else:
+                break
+        if '---stop---' == d:
+            return
+        message = pickle.loads(d)
+        return message
+
+    def send_message(self, m_in):
+        message = self.gen.buildMessage(m_in, self.sPort)
+        output = pickle.dumps(message, -1)
+        #calculate length of message and pad to four digits
+        length = str(len(output))
+        if len(length) > 4:
+            raise Exception('MessageLenException', 'this message is too long')
+        length = ''.join(['0' for x in range(4-len(length))])+length
+        print('goes out!')
+        self.s.sendall(length.encode('ascii')+output)
+
+    def send_worker(self):
+        while 1:
+            m = input()
+            if m == '---end---':
+                kill_sock()
+            else:
+                self.send_message(m)
+    def receive_worker(self):
+        while 1:
+            m = self.receive_message()
+            if m == '---end---':
+                kill_sock()
+            print(m.body)
+    def kill_sock(self):
+        self.s.shutdown(socket.SHUT_RDWR)
+        self.s.close()
 
 class ClientSock(MarvinSock):
-	def __init__(self,sPort):
-		self.gen = MessageCreator(sPort)
-		self.sPort = sPort
-		
-	def connect(self, cPort):
-		self.cPort = cPort
-		try:
-			self.conStatus = 1
-			while self.conStatus != 0:
-				time.sleep(1)
-				self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				self.conStatus = self.s.connect_ex(('localhost', self.cPort))
-			return True
-		except Exception as e:
-			print(e)
-			self.kill_sock()
+    def __init__(self,sPort):
+        self.gen = MessageCreator(sPort)
+        self.sPort = sPort
+
+    def connect(self, cPort):
+        self.cPort = cPort
+        try:
+            self.conStatus = 1
+            while self.conStatus != 0:
+                time.sleep(1)
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.conStatus = self.s.connect_ex(('localhost', self.cPort))
+            return True
+        except Exception as e:
+            print(e)
+            self.kill_sock()
 
 
 class ServerSock(MarvinSock):
-	def __init__(self,port):
-		self.port = port
-		self.gen = MessageCreator(port)
-		try:
-			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			self.s.bind(('localhost', self.port))
-			self.s.listen(5)
-		except Exception:
-			raise e
+    def __init__(self,port):
+        self.port = port
+        self.gen = MessageCreator(port)
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.bind(('localhost', self.port))
+            self.s.listen(5)
+        except Exception:
+            raise e
 
-	def getConnection(self):
-		print('wainting on port'+str(self.port))
-		conn, addr = self.s.accept()
-		print('connected on server from:'+str(addr))
-		return conn
+    def getConnection(self):
+        print('wainting on port'+str(self.port))
+        conn, addr = self.s.accept()
+        print('connected on server from:'+str(addr))
+        return conn
 
 if __name__ == '__main__':
-	marvsock = MarvinSock(int(sys.argv[1]))
-	t_rec = Thread(target=marvsock.receive_worker)
-	r_send = Thread(target=marvsock.send_worker)
-	threads = [t_rec, r_send]
-	for t in threads:
-		t.start()
-	for x in threads: 
-		x.join()
+    marvsock = MarvinSock(int(sys.argv[1]))
+    t_rec = Thread(target=marvsock.receive_worker)
+    r_send = Thread(target=marvsock.send_worker)
+    threads = [t_rec, r_send]
+    for t in threads:
+        t.start()
+    for x in threads:
+        x.join()
